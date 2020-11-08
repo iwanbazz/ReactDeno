657e75197da23b3cd96aad7547c0242ca76d1691
iwanbazz <iwan.bazz@gmail.com>
2020-11-08 20:53:41 +0700

    Add SEO tag

--- app.tsx
+++ app.tsx
@@ -1,9 +1,20 @@
 import React, { ComponentType } from 'react'
-import { Head } from 'https://deno.land/x/aleph/mod.ts'
+import { Head, SEO, Viewport } from 'https://deno.land/x/aleph/mod.ts'
 
 export default function App({ Page, pageProps }: { Page: ComponentType<any>, pageProps: any }) {
     return (
         <>
+            <SEO
+                title="React"
+                description="React framework in deno"
+                keywords="react,framework,deno,alephjs"
+            />
+            <Viewport
+                width="device-width"
+                initialScale={1.0}
+                minimumScale={1.0}
+                maximumScale={1.0}
+            />
             <Head>
                 <title>Hello World - Aleph.js</title>
             </Head>
